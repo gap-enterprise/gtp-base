3b6be87577adbdae7ac69ea57c3a336a96186604
Marx Brou <broumarcel87@gmail.com>
2022-04-06 23:35:47 +0000

    feat : Introduce notion of `AccountPec` (#19)
    Fix: #11
    
    Co-authored-by: Olivier B. OURA <baudoliver7@gmail.com>

---
+++ src/main/java/io/surati/gap/gtp/base/api/AccountPecs.java
@@ -0,0 +1,59 @@
+ /* Copyright (c) 2022 Surati
+
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to read
+ * the Software only. Permissions is hereby NOT GRANTED to use, copy, modify,
+ * merge, publish, distribute, sublicense, and/or sell copies of the Software.
+
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+package io.surati.gap.gtp.base.api;
+
+/**
+ * All Account pecs.
+ *
+ * @since 0.2
+ */
+public interface AccountPecs {
+
+    /**
+     * Get an account pec.
+     * @param code Code
+     * @return Account pec
+     */
+	AccountPec get(String code);
+
+    /**
+     * Iterate them all.
+     * @return List of Account pec
+     */
+    Iterable<AccountPec> iterate();
+
+    /**
+     * Add an account pec.
+     * @param code Code
+     * @param name Name
+     * @param notes Notes
+     */
+    void add(String code, String name, String notes);
+
+    /**
+     * Remove an account pec.
+     * @param code Code
+     */
+    void remove(String code);
+
+    /**
+     * Checks if has section with code.
+     * @param code Code
+     * @return Has or not
+     */
+	boolean has(String code);
+
+}
